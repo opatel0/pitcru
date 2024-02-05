d46f1a177a0f7a07dd573f0c3017f055a991d384
Jon Schinella <jschinel@alumni.nmu.edu>
2024-02-05 11:16:03 -0500

    Adding urls and car model

--- pitcru/main_app/urls.py
+++ pitcru/main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .import views
+from django.contrib.auth import logout
 
 urlpatterns = [
-
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('cars/', views.cars, name='cars'),
 ]
