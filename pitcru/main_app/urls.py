01198f298404cc1cfb07dab4234513811d2d0311
Jon Schinella <jschinel@alumni.nmu.edu>
2024-02-05 13:57:27 -0500

    Pushing up for standup

--- pitcru/main_app/urls.py
+++ pitcru/main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cars/', views.cars, name='cars'),
+  path('profile/', views.profile, name = 'profile'),
   path('accounts/logout/', views.logout_view, name = 'logout'),
   path('accounts/signup/', views.signup, name='signup')
 ]
