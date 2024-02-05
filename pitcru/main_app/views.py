5d74555fa1e27808e8ca08dffd41432b61c568e3
Thomas C.D. <tcdobrose@gmail.com>
2024-02-05 12:50:52 -0600

    Fixed routes

--- pitcru/main_app/views.py
+++ pitcru/main_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def home(request):
-    redirect('about/')
 
 def about(request):
     return render(request, 'about.html')
