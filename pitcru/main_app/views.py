c2e2e5ffae4c5df80e1ff8cde14c4c3c4ce39dde
Jon Schinella <jschinel@alumni.nmu.edu>
2024-02-05 11:20:06 -0500

    Adding template folder

--- pitcru/main_app/views.py
+++ pitcru/main_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Car
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+
+def cars(request):
+  return render(request, 'cars/index.html')
