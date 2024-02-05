86eefcb5004ea9aae02100f39d11ba6bedef1fd6
opatel0 <ojas.dilip.patel@gmail.com>
2024-02-05 08:12:00 -0500

    added route for details page

--- pitcru/main_app/urls.py
+++ pitcru/main_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('cars/<int:car_id>/', views.cars_detail, name='details')
 ]
--- pitcru/main_app/views.py
+++ pitcru/main_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cars_detail(request, car_id):
+    return render(request, 'cars/detail.html')
