186577d3b2e0a14b5ac0b660c993ff96a7d97f7b
Jon Schinella <jschinel@alumni.nmu.edu>
2024-02-09 13:27:13 -0500

    Changed to the user key

--- pitcru/main_app/models.py
+++ pitcru/main_app/models.py
@@ -10,7 +10,7 @@ car_list = [
 year_list = [
 "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
 API_KEY = "sDR8LF/Y92EM5TcNfaQxVg==vKgPW0X07hL86rUt"
-  
+ 
 # Create your models here.
 class Car(models.Model):
   city_mpg = models.IntegerField()
@@ -26,7 +26,7 @@ class Car(models.Model):
   transmission = models.CharField(max_length=20)
   year = models.IntegerField()
   is_featured = models.BooleanField()
-  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  user = models.ManyToManyField(User,related_name='owned_cars',on_delete=models.CASCADE)
 
   def __str__(self):
     return f"{self.year} {self.make} {self.model} ({self.id})"
